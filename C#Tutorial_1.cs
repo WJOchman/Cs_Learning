5a91ac75d7479170a10493059ecc4398ff9f3fce
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2022-01-21 17:03:15 +0000

    Add files via upload

---
+++ C#Tutorial_1.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Drawing a Shape
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            Console.ReadLine();
+        }
+    }
+}
+
+// Variables
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            string characterName = "John";
+            int characterAge = "25";
+
+            Console.WriteLine("There once was a man named" + characterName);
+            Console.WriteLine("He was" + characterAge + "years old.");
+            Console.WriteLine("He really liked the name" + characterName);
+            Console.WriteLine("But didn't like being" + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Data Types
+namespace  Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            string phrase = "Giraffe Academy";
+            char graded = 'B';
+            int age = 30;
+            float GPA = 3.1;
+            double morepreciseGPA = 3.11;
+            decimal evenmorepreciseGPA = 3.11;
+            bool isMale = false;
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Working With Strings 
