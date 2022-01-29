3f0232995f945dee3ed6323a22d58829e0c3bb07
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2022-01-29 17:44:09 +0000

    Halfway Through The Course

--- C#Tutorial_1.cs
+++ C#Tutorial_1.cs
@@ -83,14 +83,167 @@ namespace Giraffe {
             Console.WriteLine( Math.Max(4, 20) );
             Console.WriteLine( Math.Min(4, 90) );
             Console.WriteLine( Math.Round(4.6) );
-            Console.WriteLine( Math. );
 
             Console.ReadLine();
         }
     }
 }
 
+// Getting User Input
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ", you are " + age + "years old.");
+    
+            Console.ReadLine();
+        }
+    }
+}
 
+// Building a Calculator
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter a Number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
+            Console.Write("Enter Another Number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine(num1 + num2);
 
+            Console.ReadLine();
+        }
+    }
+}
+
+// Building a Mad Libs Game 
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            string colour, pluralNoun, celebrity; 
+            
+            Console.Write("Enter a colour: ");
+            colour = Console.ReadLine();
+
+            Console.Write("Enter a plural noun: ");
+            pluralNoun = Console.ReadLine();
+
+            Console.Write("Enter a celebrity name: ");
+            celebrity = Console.ReadLine();
+
+            Console.WriteLine("Roses are" + colour);
+            Console.WriteLine(pluralNoun + "are blue");
+            Console.WriteLine("I love" + celebrity);
+
+            Console.ReadLine();
+        } 
+    }
+}
+
+// Arrays 
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            int[] luckyNumbers = {4, 8, 15, 16, 23, 42};
+            Console.WriteLine(luckyNumbers[1]);    
+
+            string[] friends = new string[5]; 
+            friends[0] = "Jim";
+            friends[1] = "Kelly";
+            friends[2] = "Clarke";
+            friends[3] = "Samuel";
+            friends[4] = "Johnathan";
+            
+            Console.ReadLine();
+        }
+    }
+}
+
+// Methods
+
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            
+            SayHi("Mike", 33);
+            SayHi("John", 56);
+            SayHi("Tom", 12);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age) {
+            Console.WriteLine("Hello " + name + ", you are" + age + "years old.");
+        }
+    }
+}
+
+// Return Statement 
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(cube(5));
+
+            Console.ReadLine();
+        }
+
+        static int cube(int num) {
+            int result = num * num * num;
+            return result; 
+        }
+    }
+}
+
+// If Statements 
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+            bool isMale = true;
+            bool isTall = true;
+
+            if (isMale && isTall) {
+                Console.WriteLine("You are a tall male.");
+            } else if (isMale && !isTall){
+                Console.WriteLine("You are a short male.");
+            } else if (!isMale && isTall) {
+                Console.WriteLine("You are not male but you are tall.");
+            } else {
+                Console.WriteLine("You are not male and not tall.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// More If Satements
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(getMax(20, 10, 40));
+
+            Console.ReadLine();
+        }
+
+        static int getMax(int num1, int num2, int num3) {
+            int result; 
+            if (num1 >= num2 && num1 >= num2) {
+                result = num1;
+            } else if (num2 >= num1 && num2 >= num3) {
+                result = num2;
+            } else {
+                result = num3;
+            }
+            
+            return result;
+        }
+    }
+}
+
+// Building a Better Calculator
