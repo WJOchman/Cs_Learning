bb330b6c8573ca69b5e0dad19a76bbc6722fac8a
Wojciech Jacek Ochman <85240905+WJOchman@users.noreply.github.com>
2022-01-30 23:15:26 +0000

    Update C#Tutorial_1.cs

--- C#Tutorial_1.cs
+++ C#Tutorial_1.cs
@@ -247,3 +247,361 @@ namespace Giraffe {
 }
 
 // Building a Better Calculator
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Enter a number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());            
+
+            if (op == "+") {
+                Console.WriteLine(num1 + num2);
+            } else if (op == "=") {
+                Console.WriteLine(num1 - num2);
+            } else if (op == "*") {
+                Console.WriteLine(num1 * num2);
+            } else if (op == "/") {
+                Console.WriteLine(num1 / num2);
+            } else {
+                Console.WriteLine("Invalid operator");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Switch Statements 
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(GetDay(0));
+
+            Console.ReadLine();
+        } 
+
+        static string GetDay(int dayNum) {
+            string dayName;
+
+            switch (dayNum) {
+                case 0: dayName = "Sunday";
+                break;
+                case 1: dayName = "Monday";
+                break;
+                case 2: dayName = "Tuesday";
+                break;
+                case 3: dayName = "Wednesday";
+                break;
+                case 4: dayName = "Thursday";
+                break;
+                case 5: dayName = "Friday";
+                break;
+                case 6: dayName = "Saturday";
+                break; 
+                default: dayName = "Invalid Day Number";
+                break;
+            }
+
+            return dayName; 
+        }
+    }
+}
+
+// While Loops
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+            while (index <= 5) {
+                Console.WriteLine(index);
+                index++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Do While Loop
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+            do {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5); 
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Building a Guessing Game
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+            string secretWord = "Giraffe";
+            string guess = "";
+            int guessCount = 0;
+            int guessLimit = "3";
+            bool outOfGuesses = false;
+
+
+            while (guess != secretWord && !outOfGuesses) {
+
+                Console.WriteLine("I'm an animal in Africa with a long neck, and I love to eat leaves. What am I?");
+
+                if (guessCount < guessLimit) {
+                    Console.Write("Enter your guess: ");
+                    guess = Console.ReadLine();
+                    guessCount++;
+                } else {
+                    outOfGuesses = true;
+                }
+                
+            }
+
+            if (outOfGuesses) {
+                Console.Write("You Lose!");
+            } else {
+                Console.Write("You Win!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// For Loops
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+
+            for (int i = 1; i < luckyNumbers.Length;  i++) {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Building an Exponent Method
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(GetPow(3, 2));
+            Console.ReadLine();
+        }
+
+        static int GetPow(int baseNum, int powNum) {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++) {
+                result = result * baseNum;
+            }
+
+            return result;
+        }
+    }
+}
+
+// 2D Arrays
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+        int[,] numberGrid = {
+            {1, 2},
+            {3, 4},
+            {5, 6}
+        }; 
+        int[,] myArray = new int[2, 3];
+
+        Console.WriteLine(numberGrid[1, 1]);
+        }
+    }
+}
+
+// Exception Handling 
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+            try {
+                Console.Write("Enter a number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            } catch (DivideByZeroException) {
+                Console.WriteLine(e.Message);
+            } catch (FormatException) {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Classes & Objects
+namespace Giraffe {
+
+    class Program {
+        static void Main(string[] args) {
+
+            // Book 1
+            Book book1 = new Book();
+            book1.title = "Harry Potter";
+            book1.author = "J.K.Rowling";
+            book1.bookType = "Hard Cover";
+            book1.pages = 400;
+
+            // Book 2
+            Book book2 = new Book();
+            book2.title = "Lord of the Rings";
+            book2.author = "J.R.R.Tolkien";
+            book2.bookType = "Hard Cover";
+            book2.pages = 700;
+
+            // Here you can call the different attributes of the book classes.
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book1.bookType);
+            Console.WriteLine(book2.pages);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Book {
+        public string title;
+        public string author;
+        public string bookType;
+        public int pages;
+    }
+}
+
+// Constructors 
+namespace Giraffe {
+
+    class Program {
+        static void Main(string[] args) {
+
+            // Book 1
+            Book book1 = new Book("Harry Potter", "J.K.Rowling", "Hardcover", 400);
+
+            // Book 2
+            Book book2 = new Book("Lord of the Rings", "J.R.R.Tolkien", "Hard Cover", 700);
+
+            // Here you can call the different attributes of the book classes.
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book2.author);
+            Console.WriteLine(book1.bookType);
+            Console.WriteLine(book2.pages);
+
+            Console.ReadLine();
+        }
+    }
+
+    class Book {
+        public string title;
+        public string author;
+        public string bookType;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, string aBookType, int aPages) {
+            title = aTitle;
+            author = aAuthor;
+            bookType = aBookType;
+            pages = aPages; 
+        }
+    }
+}
+
+// Object Methods
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+            Student student1 = new Student("Jim", "Business", 2.8);
+            Student student2 = new Student("Pam", "Art", 3.6);
+
+            Console.WriteLine(student1.HasHonors());
+            Console.WriteLine(student2.HasHonors());
+
+            Console.ReadLine();
+        }
+    }
+
+    class Student {
+        public string name;
+        public string major;
+        public double gpa;
+
+        public Student(string aname, string aMajor, double aGPA);
+    }
+
+    public bool HasHonors() {
+        if (gpa >= 3.5) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// Getters & Setters
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Static Class Attributes
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Static Methods & Classes 
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
+// Inheritance
+namespace Giraffe {
+    class Program {
+        static void Main(string[] args) {
+
+
+            Console.ReadLine();
+        }
+    }
+}
